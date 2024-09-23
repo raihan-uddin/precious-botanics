82beebb9baac8f9d4b6b631776b8019b9f5c146b
raihanuddin <raihanptl21@gmail.com>
2024-09-23 19:34:30 +0600

    Add admin profile route and middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('/admin')->middleware(['auth', AdminMiddleware::class])->group(function () {
+    Route::get('/profile', function () {
+        // Your profile logic here
+    });
 });
 
 require __DIR__.'/auth.php';
