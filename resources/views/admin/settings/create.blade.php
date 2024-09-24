1f0b74bbc64669cb7929d97d78d991aadbd23f31
Md. Raihan Uddin <raihanuddin2@yahoo.com>
2024-09-25 00:04:56 +0600

    Refactor file input in create and edit settings views to accept only image files

--- resources/views/admin/settings/create.blade.php
+++ resources/views/admin/settings/create.blade.php
@@ -40,7 +40,7 @@
                                 </template>
 
                                 <template x-if="type === 'file'">
-                                    <input type="file" id="value" name="value" @change="fileChanged" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    <input type="file" id="value" name="value" @change="fileChanged" accept="image/*" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                 </template>
 
                                 <template x-if="type === 'textarea'">
