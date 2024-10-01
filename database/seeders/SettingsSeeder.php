1480402621a942a92e5609329fc3b55d0e464ad3
raihanuddin <raihanptl21@gmail.com>
2024-10-01 18:14:37 +0600

    Refactor site settings seeder with updated values

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -15,9 +15,9 @@ class SettingsSeeder extends Seeder
     {
         DB::table('settings')->insert([
             ['key' => 'site_name', 'value' => 'Precious Botanics', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_email', 'value' => '', 'type' => 'email', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_email', 'value' => 'info@preciousbotanic.com', 'type' => 'email', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_phone', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_address', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_address', 'value' => '234 E 47th street, Chicago iI 60653', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_logo', 'value' => 'logo.png', 'type' => 'file', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_favicon', 'value' => 'logo.png', 'type' => 'file', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'currency', 'value' => 'USD', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
@@ -25,18 +25,18 @@ class SettingsSeeder extends Seeder
             ['key' => 'currency_position', 'value' => 'left', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'shipping_rate', 'value' => '0', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'tax_rate', 'value' => '0', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_description', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_keywords', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_author', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_description', 'value' => 'Health & Beauty Products', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_keywords', 'value' => 'Precious Botanics, Health  & Beauty Products', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_author', 'value' => 'Precious Botanics', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_google_analytics', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_facebook', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_instagram', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_instagram', 'value' => 'https://www.instagram.com/preciousbotanic2024', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_linkedin', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_youtube', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_whatsapp', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_telegram', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
             ['key' => 'site_snapchat', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
-            ['key' => 'site_tiktok', 'value' => '', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
+            ['key' => 'site_tiktok', 'value' => 'https://www.tiktok.com/@preciousbotanic', 'type' => 'text', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
