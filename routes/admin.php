edb95926ec00bc2a05b4e2bb9e5c473e10e45f5b
raihanuddin <raihanptl21@gmail.com>
2024-10-01 18:25:59 +0600

    Refactor routes to separate admin routes into a dedicated file

---
+++ routes/admin.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BannerController;
+
+use Illuminate\Support\Facades\Route;
+
+
+// group routes with admin middleware
+Route::middleware(['auth', AdminMiddleware::class])->group(function () {
+    // Auth routes
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::prefix('/admin')->middleware(['auth', AdminMiddleware::class])->group(function () {
+        Route::resource('settings', SettingsController::class);
+        Route::resource('categories', CategoryController::class);
+        Route::resource('tags', TagController::class);
+        Route::resource('products', ProductController::class);
+        Route::resource('banners', BannerController::class);
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SettingsController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\TagController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\BannerController;
+
 use App\Http\Controllers\Frontend\PageController; 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AdminMiddleware;
@@ -13,27 +8,5 @@ use App\Http\Middleware\AdminMiddleware;
 Route::get('/', [PageController::class, 'index'])->name('home');
 
 
-
-// group routes with admin middleware
-Route::middleware(['auth', AdminMiddleware::class])->group(function () {
-    // Auth routes
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
-    Route::prefix('/admin')->middleware(['auth', AdminMiddleware::class])->group(function () {
-        Route::resource('settings', SettingsController::class);
-        Route::resource('categories', CategoryController::class);
-        Route::resource('tags', TagController::class);
-        Route::resource('products', ProductController::class);
-        Route::resource('banners', BannerController::class);
-    });
-});
-
+require __DIR__.'/admin.php';
 require __DIR__.'/auth.php';
