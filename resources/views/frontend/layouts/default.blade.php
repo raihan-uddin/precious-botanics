7aeb9bcb31fb5aae737d3ab57cd70d2b14b368e7
raihanuddin <raihanptl21@gmail.com>
2024-10-02 12:30:52 +0600

    Refactor default.blade.php to remove unused frontend assets

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -15,7 +15,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/frontend.css', 'resources/js/frontend.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js',]) // 'resources/css/frontend.css', 'resources/js/frontend.js'
 
 
         @stack('styles')
