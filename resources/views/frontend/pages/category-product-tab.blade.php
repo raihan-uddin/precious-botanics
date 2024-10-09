edd25c4acfc8b60e5b4dcc31b571e76cd4b35d91
raihanuddin <raihanptl21@gmail.com>
2024-10-09 17:45:44 +0600

    Refactor category product tab layout and styles

--- resources/views/frontend/pages/category-product-tab.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="flex flex-wrap w-full">
-    @foreach ($products as $product)
-        <div class="min-[1200px]:w-[25%] min-[768px]:w-[33.33%] w-[50%] max-[480px]:w-full px-[12px] mb-[24px]"
-            data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
-            <x-product-thumb :product="$product"/> 
-        </div>
-    @endforeach
-</div>
