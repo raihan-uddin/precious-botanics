501966bdea2596ee2f70be0890774ad1d6185254
raihanuddin <raihanptl21@gmail.com>
2024-10-15 13:38:13 +0600

    show related products

--- resources/views/frontend/pages/product-detail.blade.php
+++ resources/views/frontend/pages/product-detail.blade.php
@@ -328,83 +328,12 @@
                 <div class="w-full px-[12px]">
                     <div class="bb-deal-slider m-[-12px]">
                         <div class="bb-deal-block owl-carousel">
+                            @foreach ($relatedProducts as $product )
                             <div class="bb-deal-card p-[12px]" data-aos="fade-up" data-aos-duration="1000"
                                  data-aos-delay="200">
-                                 @foreach($relatedProducts as $product)
-                                <div
-                                    class="bb-pro-box bg-[#fff] border-[1px] border-solid border-[#eee] rounded-[20px]">
-                                    <div
-                                        class="bb-pro-img overflow-hidden relative border-b-[1px] border-solid border-[#eee] z-[4]">
-                                        <span
-                                            class="flags transition-all duration-[0.3s] ease-in-out absolute z-[5] top-[10px] left-[6px]">
-                                            <span class="text-[14px] text-[#777] font-medium uppercase">New</span>
-                                        </span>
-                                        <a href="javascript:void(0)">
-                                            <div
-                                                class="inner-img relative block overflow-hidden pointer-events-none rounded-t-[20px]">
-                                                <img class="main-img transition-all duration-[0.3s] ease-in-out w-full"
-                                                     src="assets/img/product/1.jpg" alt="product-1">
-                                                <img
-                                                    class="hover-img transition-all duration-[0.3s] ease-in-out absolute z-[2] top-[0] left-[0] opacity-[0] w-full"
-                                                    src="assets/img/product/back-1.jpg" alt="product-1">
-                                            </div>
-                                        </a>
-                                        <ul class="bb-pro-actions transition-all duration-[0.3s] ease-in-out my-[0] mx-[auto] absolute z-[9] left-[0] right-[0] bottom-[0] flex flex-row items-center justify-center opacity-[0]">
-                                            <li class="bb-btn-group transition-all duration-[0.3s] ease-in-out w-[35px] h-[35px] mx-[2px] flex items-center justify-center text-[#fff] bg-[#fff] border-[1px] border-solid border-[#eee] rounded-[10px]">
-                                                <a href="javascript:void(0)" title="Wishlist"
-                                                   class="w-[35px] h-[35px] flex items-center justify-center">
-                                                    <i class="ri-heart-line transition-all duration-[0.3s] ease-in-out text-[18px] text-[#777] leading-[10px]"></i>
-                                                </a>
-                                            </li>
-                                            <li class="bb-btn-group transition-all duration-[0.3s] ease-in-out w-[35px] h-[35px] mx-[2px] flex items-center justify-center text-[#fff] bg-[#fff] border-[1px] border-solid border-[#eee] rounded-[10px]">
-                                                <a href="javascript:void(0)" title="Quick View"
-                                                   class="bb-modal-toggle w-[35px] h-[35px] flex items-center justify-center">
-                                                    <i class="ri-eye-line transition-all duration-[0.3s] ease-in-out text-[18px] text-[#777] leading-[10px]"></i>
-                                                </a>
-                                            </li>
-                                            <li class="bb-btn-group transition-all duration-[0.3s] ease-in-out w-[35px] h-[35px] mx-[2px] flex items-center justify-center text-[#fff] bg-[#fff] border-[1px] border-solid border-[#eee] rounded-[10px]">
-                                                <a href="compare.html" title="Compare"
-                                                   class="w-[35px] h-[35px] flex items-center justify-center">
-                                                    <i class="ri-repeat-line transition-all duration-[0.3s] ease-in-out text-[18px] text-[#777] leading-[10px]"></i>
-                                                </a>
-                                            </li>
-                                            <li class="bb-btn-group transition-all duration-[0.3s] ease-in-out w-[35px] h-[35px] mx-[2px] flex items-center justify-center text-[#fff] bg-[#fff] border-[1px] border-solid border-[#eee] rounded-[10px]">
-                                                <a href="javascript:void(0)" title="Add To Cart"
-                                                   class="w-[35px] h-[35px] flex items-center justify-center">
-                                                    <i class="ri-shopping-bag-4-line transition-all duration-[0.3s] ease-in-out text-[18px] text-[#777] leading-[10px]"></i>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="bb-pro-contact p-[20px]">
-                                        <div class="bb-pro-subtitle mb-[8px] flex flex-wrap justify-between">
-                                            <a href="shop-left-sidebar-col-3.html"
-                                               class="transition-all duration-[0.3s] ease-in-out font-Poppins text-[13px] leading-[16px] text-[#777] font-light tracking-[0.03rem]">Chocos</a>
-                                            <span class="bb-pro-rating">
-                                                <i class="ri-star-fill float-left text-[15px] mr-[3px] leading-[18px] text-[#fea99a]"></i>
-                                                <i class="ri-star-fill float-left text-[15px] mr-[3px] leading-[18px] text-[#fea99a]"></i>
-                                                <i class="ri-star-fill float-left text-[15px] mr-[3px] leading-[18px] text-[#fea99a]"></i>
-                                                <i class="ri-star-fill float-left text-[15px] mr-[3px] leading-[18px] text-[#fea99a]"></i>
-                                                <i class="ri-star-line float-left text-[15px] mr-[3px] leading-[18px] text-[#777]"></i>
-                                            </span>
-                                        </div>
-                                        <h4 class="bb-pro-title mb-[8px] text-[16px] leading-[18px]">
-                                            <a href="product-left-sidebar.html"
-                                               class="transition-all duration-[0.3s] ease-in-out font-quicksand w-full block whitespace-nowrap overflow-hidden text-ellipsis text-[15px] leading-[18px] text-[#3d4750] font-semibold tracking-[0.03rem]">Mixed
-                                                Fruits Chocolates Pack</a>
-                                        </h4>
-                                        <div class="bb-price flex flex-wrap justify-between">
-                                            <div class="inner-price mx-[-3px]">
-                                                <span class="new-price px-[3px] text-[16px] text-[#686e7d] font-bold">$25</span>
-                                                <span
-                                                    class="old-price px-[3px] text-[14px] text-[#686e7d] line-through">$30</span>
-                                            </div>
-                                            <span class="last-items text-[14px] text-[#686e7d]">1 Pack</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                @endforeach
+                                 <x-product-thumb :product="$product"/>
                             </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
