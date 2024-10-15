17ea3d19b59e21e2a7b60a94e2d87c8bdf1cf19b
raihanuddin <raihanptl21@gmail.com>
2024-10-15 13:02:31 +0600

    Refactor route paths for about and contact pages

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\Frontend\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
-Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/about-us', [PageController::class, 'about'])->name('about');
 Route::get('/delivery-information', [PageController::class, 'deliveryInformation'])->name('delivery.information');
 Route::get('/privacy-policy', [PageController::class, 'privacyPolicy'])->name('privacy.policy');
 Route::get('/terms-of-service', [PageController::class, 'termsOfService'])->name('terms.of.service');
 Route::get('/return-policy', [PageController::class, 'returnPolicy'])->name('return.policy');
-Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/contact-us', [PageController::class, 'contact'])->name('contact');
 
 Route::get('/{slug}', [PageController::class, 'categoryProducts'])->name('category.products');
 Route::get('/{category_slug}/product/{slug}', [PageController::class, 'productDetail'])->name('product.detail');
